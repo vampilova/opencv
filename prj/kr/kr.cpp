be57f4ecf4e9c407bed3a8184a70f5cf4fc58b90
Erzhena Vampilova <erzhena.vampilova@gmail.com>
2019-04-09 19:35:35 +0300

    added color convert

--- prj/kr/kr.cpp
+++ prj/kr/kr.cpp
@@ -16,53 +16,60 @@ using namespace std;
 
 
 int main() {
-	Mat img(400, 400, CV_8UC1);
-	Mat res;
-	Mat res2;
-	int colorCircle = 0;
-	int colorRect = 255;
-	int k = 0;
-	int z = 0;
+    Mat img(600,600, CV_8UC1);
+    int colorCircle[] = {127,0,255,255,127,0};
+    int colorRect[] = {255,127,0,127,0,255};
+    int k, z, index =0;
+    for(int i=0;i<2;i++){
+        for(int j=0;j<3;j++){
+            if(i==1){
+                rectangle(img, Point(k,z), Point(k+img.rows/3,z+img.rows/2),Scalar(colorRect[index]),FILLED);
+                index++;
+            } else {
+                rectangle(img, Point(k,z), Point(k+img.rows/3,z+img.rows/2),Scalar(colorRect[index]),FILLED);
+                index++;
+            }
+            k+=img.rows/3;
+        }
+        z+=img.rows/2;
+        k=0;
+    }
+    index = 0;
+    for(int i=0;i<img.cols;i++){
+        for(int j=0;j<img.rows;j++){
+            
+            if ((j==img.rows/6||j==img.rows/2||j==img.rows/6*5)){
+                if(i==img.rows/4){
+                    circle(img, Point(j,i), img.rows/8, Scalar(colorCircle[index]),FILLED);
+                    index++;
+                } else if (i==img.rows/4*3){
+                    circle(img, Point(j,i), img.rows/8, Scalar(colorCircle[index]),FILLED);
+                    index++;
+                }
+            }
+        }
+    }
 
-	for (int i = 0; i < 2; i++) {
-		for (int j = 0; j < 3; j++) {
-			rectangle(img, Point(k, z), Point(k + img.rows / 3, z + img.rows / 2), Scalar(colorRect), FILLED);
-			k += img.rows / 3;
-			colorRect -= 255 / 12;    //0,128,255 (цвет белый, синий,
-		}
-		z += img.rows / 2;
-		k = 0;
-	}
-	for (int j = 0; j < img.rows; j++) {
-		for (int i = 0; i < img.cols; i++) {
-			if ((j == img.rows / 6 || j == img.rows / 2 || j == img.rows / 6 * 5) && (i == img.rows / 4 || i == img.rows / 4 * 3)) {
-				circle(img, Point(j, i), img.rows / 7, Scalar(colorCircle), FILLED);
-				colorCircle += 255 / 6;
-			}
-		}
-	}
-
-	imshow("res", img);
-
-	const float kernelData[] = {
-		1.0f, 0,
-		0, -1.0f };
-	const Mat kernel(2, 2, CV_32FC1, (float *)kernelData);
-	filter2D(img, res, -1, kernel);
-
-	imshow("after filter", res);
-
-	const float kernelData2[] = {
-		0, 1.0f,
-		-1.0f, 0 };
-	const Mat kernel2(2, 2, CV_32FC1, (float *)kernelData2);
-	filter2D(img, res2, -1, kernel2);
-
-	imshow("after filter2", res2);
-
-	waitKey();
-	destroyAllWindows();
-	return 0;
+    Mat resFirst;
+    Mat resSecond;
+    const float kernelData1[] = {
+        1.0, 0,
+        0, -1.0};
+    const Mat kernel1(2, 2, CV_32FC1,(float *)kernelData1);
+    filter2D(img, resFirst, -1, kernel1, Point(-1, -1), 128, BORDER_REPLICATE);
+    
+    const float kernelData2[] = {
+        0, 1.0,
+        -1.0, 0};
+    const Mat kernel2(2, 2, CV_32FC1,(float *)kernelData2);
+    filter2D(img, resSecond, -1, kernel2, Point(-1, -1), 128, BORDER_REPLICATE);
+    
+    imshow("after filter1", resFirst);
+    imshow("after filter2", resSecond);
+    imshow("result",img);
+    waitKey();
+    destroyAllWindows();
+    return 0;
 }
 
 
